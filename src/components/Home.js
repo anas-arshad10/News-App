168e4a852f4fe79c38c1e4e5586b39862aecf029
anas-arshad10 <anas.arshad10@gmail.com>
2023-06-07 21:39:54 +0500

    News Website

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Home = () => {
+
+  return (
+    <div className='text-center'>
+      <h1>This IS NEWS HOME PAGE</h1>
+    </div>
+  )
+
+}
+
+export default Home
